47e7e5db4825cb90a76587de4cedb961c39f02e4
holistudio <daniel.bin.lu@gmail.com>
2019-01-14 19:20:23 -0500

    categories and items recorded as JSON objects and drawn accordingly
    all drawing takes place after the key drawing points are calculated.

--- static/sketch.js
+++ static/sketch.js
@@ -6,7 +6,7 @@ var categoryNames = [
   'physical design',
   'interface',
   'occupant initiative'];
-var categoryDia = 400;
+var categoryDia = 300;
 var itemDia = 10;
 var archive={}; //JSON so that the keys can be strings
 /* {
@@ -24,7 +24,9 @@ var archive={}; //JSON so that the keys can be strings
       category:'Design Principles',
       tags: 'foo1,foo2',
       notes: '',
-      coordinates: {x:30, y:32}},]
+      coordinates: {x:30, y:32}},
+      catLineEndPts: [{x:5,y:10},...],
+      tagLineEndPts: []},...]
     },
     "hardware":
     {name: 'Hardware',
@@ -74,16 +76,23 @@ function preload() {
 
 
 function setup() {
+
   //for each row in the table,
   for (let i=0; i<csvTable.getRowCount(); i++) {
     //get the row's category
-    var itemCategory = csvTable.getString(i,'category');
+    const itemCategory = csvTable.getString(i,'category');
     //if that category isn't in archive, add it to archive
     if(archive[itemCategory] == undefined){
       archive[itemCategory]={"name": itemCategory, "numItems": 0, "items":[]}
     }
     //add the item to appropriate category's items list
     archive[itemCategory].items.push(csvTable.getRow(i).obj);
+
+    //initialize the other item object properties
+    var itemIndex=archive[itemCategory].numItems;
+    archive[itemCategory].items[itemIndex]["coordinates"]={};
+    archive[itemCategory].items[itemIndex]["catLineEndPts"]=[];
+    archive[itemCategory].items[itemIndex]["tagLineEndPts"]=[];
 
     //increment category's numItems by 1
     archive[itemCategory].numItems++;
@@ -96,6 +105,7 @@ function setup() {
     }
   }
 
+  createCanvas(1150, 768);
   //calculate where the category abd item circle centers are
   //2 x 3 grid for the six categories
   var numRows = 2;
@@ -108,15 +118,19 @@ function setup() {
   let c=0;
   for (category in archive) {
     //category diameter is in proportion to the number of items
-    archive[category]["diameter"] = categoryDia * archive[category].numItems / maxNumItems;
+    archive[category]["diameter"] = map(archive[category].numItems,0,maxNumItems,100,categoryDia);
     archive[category]["coordinates"] = {x: startX+c*gridX, y:startY+r*gridY}
+
+    //for each item in the category
+    //place it randomly within the category circle
     for (let i=0; i<archive[category].items.length; i++) {
       const radius = random((archive[category].diameter-itemDia)/2);
       const theta = random(TWO_PI);
-      archive[category].items[i]["coordinates"]={x:archive[category].coordinates.x + radius*cos(theta),
+      archive[category].items[i]["coordinates"]={
+        x:archive[category].coordinates.x + radius*cos(theta),
         y: archive[category].coordinates.y + radius*sin(theta)};
     }
-    if (c<numCols) {
+    if (c<numCols-1) {
       c++;
     }
     else {
@@ -124,73 +138,93 @@ function setup() {
       r++;
     }
   }
-  console.log(archive);
+
+  //calculate endpoints for lines between items and other categories
+  for (category in archive) {
+    //for each item in the category
+    for (let i=0; i<archive[category].items.length; i++) {
+      const item = archive[category].items[i];
+      //if that item has a tag sharing another category name, draw a line between that item
+      //and the category's coordinates
+      for (otherCategory in archive) {
+        if (otherCategory != category){
+          if(item.tags.includes(otherCategory)){
+            item.catLineEndPts.push(archive[otherCategory].coordinates);
+          }
+        }
+      }
+    }
+  }
+
   textFont("Gill Sans MT");
-  createCanvas(1150, 768);
-  background(225);
+
+  background(0.99*255);
 
 
   noLoop();
 }
 
-// function draw() {
-//   //for each category
-//   fill(0.7*255);
-//   noStroke();
-//   for(let i=0; i<categoryNames.length; i++){
-//   	//draw  a circle of categoryDia
-//     ellipse(gridPoints[i][0],gridPoints[i][1],categoryDia ,categoryDia );
-//
-//   	// with text at the center of that circle
-//     push();
-//     fill(0);
-//     textSize(24);
-//     textAlign(CENTER);
-//     rectMode(CENTER);
-//     text(categoryNames[i].toUpperCase(),gridPoints[i][0],gridPoints[i][1],200,72);
-//     pop();
-//
-//   }
-//   //place tagName text in the center of the screen
-//   push();
-//   fill(0);
-//   textSize(24);
-//   textAlign(CENTER);
-//   rectMode(CENTER);
-//   text(tagName,width/2,height/2);
-//   pop();
-//
-//   //Title,Date,Type,URL,Description,Category,Tags,Notes
-//
-//   //for each item
-//   for(let i=0; i<csvTable.getRowCount(); i++){
-//     var itemCategory = csvTable.getString(i,'Category');
-//
-//
-//     //(within the category circle)
-//   	//if that item has a tag sharing another category name, draw a line between that item
-//     //and the category
-//     strokeWeight(1);
-//     stroke(255, 0, 0);
-//     for(let j=0; j<categoryNames.length; j++){
-//       if(categoryNames[j]!=itemCategory){
-//         if(csvTable.getString(i,'Tags').includes(categoryNames[j])){
-//         }
-//
-//       }
-//     }
-//     stroke(0, 0, 255);
-//     //if that item has a tag sharing tagName, draw a line between that item
-//     if(csvTable.getString(i,'Tags').includes(tagName)){
-//       line(loc[0],loc[1],width/2,height/2);
-//     }
-//     noStroke();
-//   	//then draw the solid circle representing the item
-//     fill(0);
-//     ellipse(loc[0],loc[1],10,10);
-//
-//
-//
-//   }
-//
-// }
+function draw() {
+  //draw category circles
+  for (category in archive) {
+    // fill(0.7*255); hover color
+    fill(0.9*255)
+    noStroke();
+    //draw  a circle of categoryDia
+    ellipse(archive[category].coordinates.x,archive[category].coordinates.y,archive[category].diameter,archive[category].diameter);
+
+
+
+    fill(0)
+    //draw category labels outside the circles
+    textSize(24);
+    textAlign(CENTER);
+    rectMode(CENTER);
+
+    text(archive[category].name.toUpperCase(),archive[category].coordinates.x,archive[category].coordinates.y-archive[category].diameter/2,200,72);
+  }
+
+  strokeWeight(1);
+  stroke(0, 0, 0, 0.1*255);
+  for (category in archive) {
+    //for each item in the category
+    for (let i=0; i<archive[category].items.length; i++) {
+      const item = archive[category].items[i];
+      //if that item has a tag sharing another category name, draw a line between that item
+      //and the category's coordinates
+
+      for (let j=0; j<item.catLineEndPts.length; j++) {
+        line(item.coordinates.x,item.coordinates.y,item.catLineEndPts[j].x,item.catLineEndPts[j].y);
+      }
+    }
+  }
+
+  noStroke();
+  fill(0);
+  for (category in archive) {
+    //for each item in the category
+    for (let i=0; i<archive[category].items.length; i++) {
+      const item = archive[category].items[i];
+
+      //draw items
+      ellipse(item.coordinates.x,item.coordinates.y,itemDia,itemDia);
+    }
+  }
+  //place tagName text in the on top of screen
+  // push();
+  // fill(0);
+  // textSize(24);
+  // textAlign(CENTER);
+  // rectMode(CENTER);
+  // text(tagName,width/2,height/2);
+  // pop();
+
+  //if that item has a tag sharing tagName, draw a line between that item
+  // stroke(0, 0, 255);
+  // if(csvTable.getString(i,'Tags').includes(tagName)){
+  //   line(loc[0],loc[1],width/2,height/2);
+  // }
+
+
+
+}
