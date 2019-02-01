ade777484d98cf50409e993a1db9df3af2ad2dfe
Daniel <daniel.bin.lu@gmail.com>
2019-01-31 22:58:56 -0500

    initial admin view on items/categories/tags

--- mysite/neuron/admin.py
+++ mysite/neuron/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, Category, Tag
+
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('title', 'authors', 'type', 'category');
+	fields = ['title', 'authors', 'type', 'url'];
+
+class ItemInline(admin.StackedInline):
+	model = Item;
+	extra = 0;
+	fields = ['title', 'authors', 'type', 'url'];
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name'];
+    fields = ['name'];
+class CategoryAdmin(admin.ModelAdmin):
+	list_display=['name'];
+	fields = ['name'];
+	#display all order items
+	inlines = [ItemInline];
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
