05694c5ae6bea6341e3654bfa09c5e60ce72881d
Daniel <daniel.bin.lu@gmail.com>
2019-02-20 14:37:29 -0500

    handle remove item button click

--- mysite/neuron/views.py
+++ mysite/neuron/views.py
@@ -33,45 +33,53 @@ def index(request):
 
 def item_update(request):
 	if request.method == 'POST':
-		#find item's corresponding category or create one if new category
-		category, created = Category.objects.get_or_create(
-		name=request.POST['item_category']);
+		#if delete item is in the keys of the request
+		if 'delete_item' in request.POST.keys():
+			#delete the item instead of updating
+			print(request.POST['delete_item']));
+			item = Item.objects.get(id = request.POST["item_id"]);
+			item.delete();
+			item.category.save();
+		else:
+			#find item's corresponding category or create one if new category
+			category, created = Category.objects.get_or_create(
+			name=request.POST['item_category']);
 
-		#check through all tags
-		#split tag list into array
-		tag_list = request.POST['item_tags'].split(',');
-		tag_obj_list = [];
+			#check through all tags
+			#split tag list into array
+			tag_list = request.POST['item_tags'].split(',');
+			tag_obj_list = [];
 
-		#for each tag in that array get or create
-		for tag in tag_list:
-			tag_obj, created = Tag.objects.get_or_create(
-			name=tag.strip());
-			#then add to another array of tag objects
-			tag_obj_list.append(tag_obj);
+			#for each tag in that array get or create
+			for tag in tag_list:
+				tag_obj, created = Tag.objects.get_or_create(
+				name=tag.strip());
+				#then add to another array of tag objects
+				tag_obj_list.append(tag_obj);
 
-		#get the item with id from form
-		#if the item id is not found, it's a new item and is created
-		#in that case, the new item is automatically assigned the category
-		#in the form.
-		item, created = Item.objects.get_or_create(id = request.POST["item_id"],
-		defaults = {'category': category});
-		item_dict = model_to_dict(item);
-		#for each model field of Item, update with the corresponding
-		#POST request object
-		for field in Item._meta.get_fields():
-			#if the field is category set to category object instead
-			if field.name == 'category':
-				setattr(item,field.name,category);
-			elif field.name =='tags':
-				for tag_obj in tag_obj_list:
-					item.tags.add(tag_obj);
-			elif field.name == 'date':
-				if request.POST['item_date']!='':
-					datetime_conv = date.fromisoformat(request.POST['item_date']);
-					setattr(item,field.name,datetime_conv);
-			else:
-				request_key = str(f"item_{field.name}");
-				setattr(item,field.name,request.POST[request_key]);
-		item.save();
+			#get the item with id from form
+			#if the item id is not found, it's a new item and is created
+			#in that case, the new item is automatically assigned the category
+			#in the form.
+			item, created = Item.objects.get_or_create(id = request.POST["item_id"],
+			defaults = {'category': category});
+			item_dict = model_to_dict(item);
+			#for each model field of Item, update with the corresponding
+			#POST request object
+			for field in Item._meta.get_fields():
+				#if the field is category set to category object instead
+				if field.name == 'category':
+					setattr(item,field.name,category);
+				elif field.name =='tags':
+					for tag_obj in tag_obj_list:
+						item.tags.add(tag_obj);
+				elif field.name == 'date':
+					if request.POST['item_date']!='':
+						datetime_conv = date.fromisoformat(request.POST['item_date']);
+						setattr(item,field.name,datetime_conv);
+				else:
+					request_key = str(f"item_{field.name}");
+					setattr(item,field.name,request.POST[request_key]);
+			item.save();
 	#redirect to index
 	return HttpResponseRedirect(reverse("neuron:index"));
